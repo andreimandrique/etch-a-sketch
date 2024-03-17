ec2e8d52f31a9122328ba55479b8d8318ae7317d
Kiyotaka327 <andreimandrique3@gmail.com>
2024-03-18 00:12:39 +0800

    add comments to js file

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const btnBoxNumber = document.getElementById("btnBoxNumber");
+//get the container and reset button
+const btnReset = document.getElementById("btnReset");
 const container = document.getElementById("container");
 
-btnBoxNumber.addEventListener("click", () =>{
+//generate 108 div box
+for(let i = 1; i <= 108 ; i++){
 
+    //make a div named box
+    const box = document.createElement("div");
+
+    //add class box-design to the box
+    box.classList.add("box-design");
+
+    //append the box dive to the container
+    container.appendChild(box);
+
+    //add event listener to each box
+    box.addEventListener("mouseover", (event)=>{
+
+        //generate random RGB
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+
+        //change the background-color of the div box to a random RGB
+        event.target.style.cssText = 
+        `background-color: rgb(${red},${green},${blue})`;
+    })
+}
+
+//add event listener to the reset button
+btnReset.addEventListener("click", () =>{
+
+    //get all box div with a class box-design
     const allBoxDesign = document.querySelectorAll(".box-design");
 
+    //change all box div color to white using forEach method
     allBoxDesign.forEach(element=>{
         element.style.cssText = 
         "background-color: white";
     })
 })
-
-for(let i = 1; i <= 108 ; i++){
-
-    const box = document.createElement("div");
-    box.classList.add("box-design");
-    container.appendChild(box);
-
-    box.addEventListener("mouseover", (event)=>{
-
-        const red = Math.floor(Math.random() * 256);
-        const green = Math.floor(Math.random() * 256);
-        const blue = Math.floor(Math.random() * 256);
-
-        event.target.style.cssText = 
-        `background-color: rgb(${red},${green},${blue})`;
-    })
-}
